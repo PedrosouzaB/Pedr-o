9fc0e950f071623d357bab14a20658676d0c64f0
PedrosouzaB <guilhermebrunner2@gmail.com>
2025-06-11 10:21:29 -0300

    Update vasco.3.py

--- vasco.3.py
+++ vasco.3.py
@@ -1,11 +1,11 @@
 nome = input("Qual é o seu nome?")
 Temperatura = float(input("Qual é a temperatura de onde voce está?"))
-if Temperatura >= 40:
+if Temperatura >=20 and Temperatura <= 40:
     print(f"{nome} Onde tu está tá muitooo quenteeee")
 
-elif Temperatura <= 20:
+elif Temperatura <=20 and Temperatura >= 5:
     print(f"{nome} Onde tu está tá muitooooo frioooo")
-elif Temperatura <= 5:
+elif Temperatura <=5 and Temperatura >=0 :
     print(f"{nome} tá geladooooo pra caralhooo")
 else:
     print(f"{nome} se vai morrerrrrrr se ferrrouuuu")
